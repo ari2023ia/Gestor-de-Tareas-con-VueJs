ac8d67cf377a5f99e09f12d0b3564bf2ff04277a
ari2023ia <ari2023ia@gmail.com>
2024-11-19 10:39:47 -0400

    3ero

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { ghPages } from 'vite-plugin-gh-pages'
 export default defineConfig({
   plugins: [
     vue({
@@ -9,7 +9,8 @@ export default defineConfig({
           runtimeCompiler: true
         }
       }
-    })
+    }),
+    ghPages()
   ],
   resolve: {
     alias: {
